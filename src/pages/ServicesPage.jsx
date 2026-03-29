5efe3ecdba9ccc9cba3a003f5d60db23ad793efc
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-30 03:52:28 +0530

    Create ServicesPage.jsx

---
+++ src/pages/ServicesPage.jsx
@@ -0,0 +1,10 @@
+import Services from "../components/Services";
+
+export default function ServicesPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h2>Our Services</h2>
+      <Services />
+    </div>
+  );
+}
