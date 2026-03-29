445976010f4b1bc06dc70e81de7f520f89e91e54
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-30 02:44:43 +0530

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,29 +1,22 @@
 export default function Services() {
   return (
-    <div className="container">
-      <h2>Our Services</h2>
+    <div className="services">
 
-      <div style={{ display: "flex", gap: 20 }}>
-        
-        <div className="card">
-          <img src="/images/pickup.png" width="60" />
-          <h3>Pickup</h3>
-          <p>We collect clothes from your home</p>
-        </div>
-
-        <div className="card">
-          <img src="/images/ironing.png" width="60" />
-          <h3>Ironing</h3>
-          <p>Professional ironing service</p>
-        </div>
-
-        <div className="card">
-          <img src="/images/delivery.png" width="60" />
-          <h3>Delivery</h3>
-          <p>Delivered back neatly</p>
-        </div>
-
+      <div className="service-item">
+        <img src="/images/truck.png" />
+        <p>Free Pickup & Delivery</p>
       </div>
+
+      <div className="service-item">
+        <img src="/images/ironing.png" />
+        <p>Quick & Reliable</p>
+      </div>
+
+      <div className="service-item">
+        <img src="/images/hanger.png" />
+        <p>Satisfaction Guaranteed</p>
+      </div>
+
     </div>
   );
 }
