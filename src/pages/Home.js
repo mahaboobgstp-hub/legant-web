66ed13e6c79037c41f11d0fcb83b053f760e0eec
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-29 22:42:05 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,16 @@
+import { useNavigate } from "react-router-dom";
 
+export default function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Legant</h1>
+      <p>Premium Ironing Service</p>
+
+      <button onClick={() => navigate("/book")}>
+        Book Pickup
+      </button>
+    </div>
+  );
+}
