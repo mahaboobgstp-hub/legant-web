884cdf9a5c0c903bfc27576cc8dc8b51e2838444
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-30 03:41:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCdysVRaELP5d5Bd8h6_hktiJaKYywRm2k",
@@ -16,3 +18,4 @@ const app = initializeApp(firebaseConfig);
 // ✅ THESE ARE REQUIRED
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const auth = getAuth(app);
