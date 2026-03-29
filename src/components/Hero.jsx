33fe4fbf9af509a3eeb70354ff64950612f9ef0a
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-30 04:28:54 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
 
         <p>Pickup & Delivery Available!</p>
 
-        <button className="btn">Schedule a Pickup</button>
+        <button className="btn">Book Pickup</button>
       </div>
 
       <div className="hero-right">
