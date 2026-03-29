60947bc26deffce64128bcedde08616c35ab9043
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-29 23:20:35 +0530

    Update OrderSummary.jsx

--- src/pages/OrderSummary.jsx
+++ src/pages/OrderSummary.jsx
@@ -1,11 +1,14 @@
 export default function OrderSummary() {
   return (
-    <div style={{ padding: 20 }}>
-      <h2>Order Confirmed ✅</h2>
+    <div className="container">
+      <div className="card">
+        <h2>Order Confirmed ✅</h2>
+        <p>Your clothes will be picked up soon</p>
 
-      <a href="https://wa.me/91XXXXXXXXXX?text=Order%20confirmed">
-        Contact on WhatsApp
-      </a>
+        <a href="https://wa.me/91XXXXXXXXXX">
+          Contact on WhatsApp
+        </a>
+      </div>
     </div>
   );
 }
