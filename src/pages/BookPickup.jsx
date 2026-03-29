ebd5a479ce470d66bd4c2653e8d2d08224ea6335
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-29 23:33:05 +0530

    Update BookPickup.jsx

--- src/pages/BookPickup.jsx
+++ src/pages/BookPickup.jsx
@@ -7,20 +7,28 @@ export default function BookPickup() {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [items, setItems] = useState("");
-
+  const [imageUrl, setImageUrl] = useState("");
+  
   const submitOrder = async () => {
+  try {
     await addDoc(collection(db, "orders"), {
       name,
       phone,
       items,
+      imageUrl,   // ✅ This stores photo URL
       status: "Pickup Scheduled",
       createdAt: new Date()
     });
 
-    alert("Order placed!");
-  };
+    alert("Order placed successfully!");
 
-  return (
+  } catch (error) {
+    console.error(error);
+    alert("Error placing order");
+  }
+};
+
+    return (
     <div className="container">
       <div className="card">
         <h2>Book Pickup</h2>
@@ -28,6 +36,15 @@ export default function BookPickup() {
         <input placeholder="Name" onChange={e => setName(e.target.value)} /><br /><br />
         <input placeholder="Phone" onChange={e => setPhone(e.target.value)} /><br /><br />
         <input placeholder="Clothes Count" onChange={e => setItems(e.target.value)} /><br /><br />
+
+
+        <h4>Upload Clothes Photo</h4>
+
+<PhotoUpload setImageUrl={setImageUrl} />
+
+{imageUrl && (
+  <img src={imageUrl} width="150" />
+)}
 
         <button className="btn" onClick={submitOrder}>
           Confirm Pickup
