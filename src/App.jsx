155cbb2e18116e75924a1d81c146496185f13653
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-30 03:30:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
-import BookPickup from "./pages/BookPickup";
-import OrderSummary from "./pages/OrderSummary";
-import Navbar from "./components/Navbar";
-import Admin from "./pages/Admin";
+import Book from "./pages/Book";
+import Track from "./pages/Track";
 import Login from "./pages/Login";
 
-function App() {
+import Navbar from "./components/Navbar";
+
+export default function App() {
   return (
     <BrowserRouter>
+
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/book" element={<BookPickup />} />
-        <Route path="/summary" element={<OrderSummary />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/book" element={<Book />} />
+        <Route path="/track" element={<Track />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
-
-export default App;
