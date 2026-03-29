d3d2f65be447e21990d9bcb47602093e1c8b2cf9
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-29 23:17:22 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+import "../assets/styles.css";
+
 export default function Navbar() {
   return (
-    <div style={{ padding: 10, background: "#eee" }}>
-      <h3>Legant</h3>
+    <div style={{
+      display: "flex",
+      justifyContent: "space-between",
+      padding: "15px 40px",
+      background: "white"
+    }}>
+      <h2>Legant</h2>
+
+      <div>
+        <Link to="/" style={{ marginRight: 20 }}>Home</Link>
+        <Link to="/book">Book</Link>
+      </div>
     </div>
   );
 }
