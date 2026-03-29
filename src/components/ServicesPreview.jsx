b79b80fc1bb866fb53ab06ec236770c70db81866
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-30 04:00:33 +0530

    Create ServicesPreview.jsx

---
+++ src/components/ServicesPreview.jsx
@@ -0,0 +1,22 @@
+export default function ServicesPreview() {
+  return (
+    <div className="services">
+
+      <div className="service-item">
+        <img src="/images/pickup.png" />
+        <p>Free Pickup & Delivery</p>
+      </div>
+
+      <div className="service-item">
+        <img src="/images/ironing.png" />
+        <p>Quick & Reliable</p>
+      </div>
+
+      <div className="service-item">
+        <img src="/images/delivery.png" />
+        <p>Satisfaction Guaranteed</p>
+      </div>
+
+    </div>
+  );
+}
