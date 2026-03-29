bf3411473938b8882a95d9f5f4031f6b7ab892ab
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-29 22:56:20 +0530

    Update OrderSummary.jsx

--- src/pages/OrderSummary.jsx
+++ src/pages/OrderSummary.jsx
@@ -2,12 +2,8 @@ export default function OrderSummary() {
   return (
     <div style={{ padding: 20 }}>
       <h2>Order Confirmed ✅</h2>
-      <p>Your clothes will be picked up soon.</p>
 
-      <a
-        href="https://wa.me/91XXXXXXXXXX?text=My%20order%20details"
-        target="_blank"
-      >
+      <a href="https://wa.me/91XXXXXXXXXX?text=Order%20confirmed">
         Contact on WhatsApp
       </a>
     </div>
