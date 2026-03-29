630390bc2c85149ff8a5b2a2a5b631b57fc146bb
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-29 23:52:53 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,16 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET"
+  apiKey: "AIzaSyBnqeAvsQcYhnu5JDw8B1Qz_8_AMzQp-oU",
+  authDomain: "legant-ac98f.firebaseapp.com",
+  projectId: "legant-ac98f",
+  storageBucket: "legant-ac98f.firebasestorage.app",
+  messagingSenderId: "722038074359",
+  appId: "1:722038074359:web:bb4786c1a11644e14d9cf2"
 };
 
 const app = initializeApp(firebaseConfig);
 
+// ✅ THESE TWO ARE VERY IMPORTANT
 export const db = getFirestore(app);
 export const storage = getStorage(app);
