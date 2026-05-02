f715231f2470c1c05c1dd53e9f5dd9ad43408a92
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-05-03 01:37:57 +0530

    Update Pricing.jsx

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import "../assets/pricing.css";
 
 export default function Pricing() {
